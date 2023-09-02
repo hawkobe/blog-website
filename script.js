7b5a660c92adb20b6999a3f132de48008aa96f1a
hawkobe <sanders.jms@gmail.com>
2023-09-02 17:44:08 -0600

    added js functionality for swiper

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ new Masonry("#posts .grid", {
   itemSelector:".grid-item",
   gutter: 20
 });
+
+// swiper library initialization
+
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  autoplay: {
+    delay: 3000
+  },
+  // responsive breakpoints
+  breakpoints: {
+    '@0': {
+      slidesPerView: 2
+    },
+    //888px
+    '@1.00': {
+      slidesPerView: 3
+    },
+    //1110px
+    '@1.25': {
+      slidesPerView: 4
+    },
+    //1330px
+    '@1.50': {
+      slidesPerView: 5
+    }
+  }
+});
